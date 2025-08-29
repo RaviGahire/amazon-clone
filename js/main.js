aa8a9e0a1c37222becb2119754b0cc1793923144
RaviGahire <ravigahire3@gmail.com>
2025-08-30 00:13:08 +0530

    Css problem fix

--- js/main.js
+++ js/main.js
@@ -42,26 +42,12 @@ navCloseBtn.addEventListener('click', () => {
 });
 
 // Home page carousel
-// const track = document.querySelector(".carousel-track");
-// const slides = document.querySelectorAll(".carousel-slide");
-// const prevBtn = document.querySelector(".prev");
-// const nextBtn = document.querySelector(".next");
+const track = document.querySelector(".carousel-track");
+const slides = document.querySelectorAll(".carousel-slide");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
 
-// let index = 0;
 
-// function showSlide(i) {
-//     if (i < 0) index = slides.length - 1;
-//     else if (i >= slides.length) index = 0;
-//     else index = i;
-
-//     track.style.transform = `translateX(-${index * 100}%)`;
-// }
-
-// nextBtn.addEventListener("click", () => showSlide(index + 1));
-// prevBtn.addEventListener("click", () => showSlide(index - 1));
-
-// Auto-slide every 4 seconds
-// setInterval(() => showSlide(index + 1), 4000);
 
 
 
